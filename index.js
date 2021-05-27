c4146f55f02422cd62139aba28ed543373edb92b
Branden <brandenehman45@gmail.com>
2021-05-27 17:01:47 -0600

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-
+function superbowlWin(record) {
+    const result = record.find((object) => object.result === 'W')
+        if(result) {
+            const year = result.year
+            return year
+        }else {
+            return undefined
+        }
+}
